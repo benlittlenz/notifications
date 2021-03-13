a35f461dda0f1460851522dba9417447c8a673aa
benlittlenz <ben@pop-os.localdomain>
2021-03-14 08:58:57 +1300

    paginating and transforming sms transactions

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,17 +2,30 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Message;
 use Inertia\Inertia;
+use App\Models\Message;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class MessageController extends Controller
 {
     public function index()
     {
-
         return Inertia::render('Transactions/Index', [
-            'transactions' => Message::all()
+            'transactions' => Auth::user()->account->messages()
+                ->orderBy('date')
+                ->paginate(25)
+                ->transform(function ($transaction) {
+                    return [
+                        'id' => $transaction->id,
+                        'date' => $transaction->date,
+                        'from' => $transaction->from,
+                        'too' => $transaction->too,
+                        'message' => $transaction->message,
+                        'source' => $transaction->source,
+                        'status' => $transaction->status,
+                    ];
+                }),
         ]);
     }
 }
